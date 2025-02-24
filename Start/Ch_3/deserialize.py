00db2725ead39ec155d0921f294a36b15949a5b4
Nikola D Trajic <ntrajic@users.noreply.github.com>
2025-02-24 06:35:58 +0000

    chapter3

--- Start/Ch_3/deserialize.py
+++ Start/Ch_3/deserialize.py
@@ -9,6 +9,34 @@ import pprint
 result = []
 
 # TODO: open the CSV file for reading
+# -----------------------------------------
+# read the contents of a CSV file into an object structure
+result = []
 
+# open the CSV file for reading
+
+with open("largequakes.csv", "r") as csvfile:
+    # create the reader object
+    reader = csv.reader(csvfile)
+
+    # does the file contain headers?
+    sniffer = csv.Sniffer()
+    sample = csvfile.read(1024)
+    csvfile.seek(0)
+    if (sniffer.has_header(sample)):
+        next(reader)
+
+    # iterate over each row
+    for row in reader:
+        # print(row)
+
+        # add the data to our list
+        result.append({
+            "place": row[0],
+            "magnitude": row[1],
+            "date": row[2],
+            "link": row[3]
+        })
+# -----------------------------------------
 
 pprint.pp(result)
